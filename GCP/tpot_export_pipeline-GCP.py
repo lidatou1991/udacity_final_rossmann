9a8428063ad2071e0981124a8ff5e463534855fc
li mao <limao@lideMacBook-Pro.lan>
2019-03-16 23:19:48 +0800

    gcp files uploaded

--- GCP/tpot_export_pipeline-GCP.py
+++ GCP/tpot_export_pipeline-GCP.py
@@ -1,11 +1,8 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import RidgeCV
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.pipeline import make_pipeline, make_union
 from sklearn.preprocessing import Imputer
-from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -18,11 +15,8 @@ imputer.fit(training_features)
 training_features = imputer.transform(training_features)
 testing_features = imputer.transform(testing_features)
 
-# Average CV score on the training set was:-725.3102436489856
-exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=RidgeCV()),
-    KNeighborsRegressor(n_neighbors=37, p=1, weights="distance")
-)
+# Average CV score on the training set was:-1615.966779868347
+exported_pipeline = GradientBoostingRegressor(alpha=0.85, learning_rate=0.5, loss="ls", max_depth=3, max_features=0.05, min_samples_leaf=3, min_samples_split=3, n_estimators=100, subsample=0.35000000000000003)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
